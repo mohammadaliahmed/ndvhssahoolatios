6cdf2feb3cf2b3df773c75905bd5b7e7a89e564a
waseem <waseemelahi35@gmail.com>
2021-02-03 12:22:10 +0500

    a

--- ndvhssahoolat/ViewController.swift
+++ ndvhssahoolat/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDataSource{
 
+    @IBOutlet var table:UITableView!
+    let data=["First","Second","Third","Fourth"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        table.dataSource=self
         // Do any additional setup after loading the view.
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
 
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "cell",for:indexPath)
+        cell.textLabel?.text=data[indexPath.row]
+        return cell
+    }
 
 }
 
